fd1c06bf7a2fe3f3e22a91ab8768eddc62deea88
baeksy <yubi100@daum.net>
2024-03-08 09:51:23 +0900

    [개선사항] Ospec Import  BOM 환경에서만 실행할 수 있도록 메세지 처리함

--- com.symc.plm.rac.prebom/src/com/symc/plm/rac/prebom/preospec/commands/PreOSpecImportCommand.java
+++ com.symc.plm.rac.prebom/src/com/symc/plm/rac/prebom/preospec/commands/PreOSpecImportCommand.java
@@ -4,6 +4,7 @@ import com.ssangyong.common.utils.CustomUtil;
 import com.symc.plm.rac.prebom.common.TypeConstant;
 import com.symc.plm.rac.prebom.preospec.dialog.PreOSpecImportDlg;
 import com.teamcenter.rac.aif.AbstractAIFCommand;
+import com.teamcenter.rac.aif.AbstractAIFUIApplication;
 import com.teamcenter.rac.aif.kernel.InterfaceAIFComponent;
 import com.teamcenter.rac.aifrcp.AIFUtility;
 import com.teamcenter.rac.kernel.TCComponentBOMLine;
@@ -26,6 +27,17 @@ public class PreOSpecImportCommand extends AbstractAIFCommand {
 	private boolean targetCheck() throws Exception {
 	    try
 	    {
+	    	AbstractAIFUIApplication currentApplication = AIFUtility.getCurrentApplication();
+	    	
+	    	String appId = currentApplication.getApplicationId();
+	    	//[20240308][UPGRADE] BOM 구조에서만 실행되도록 함
+	    	if(!"com.teamcenter.rac.pse.PSEApplication".equals(appId) && !"com.teamcenter.rac.cme.mpp.MPPApplication".equals(appId) 
+	    			&& !"com.teamcenter.rac.cme.mbm.mbmApplication".equals(appId))
+	    	{
+	    		MessageBox.post(AIFUtility.getActiveDesktop(), "This menu cannot be executed in the current application.", "INFO", MessageBox.INFORMATION);
+	    		return false;
+	    	}
+	    	
 	        targets = CustomUtil.getTargets();
 
     	    if (targets == null || targets.length != 1 || ! (targets[0] instanceof TCComponentBOMLine) || ! ((TCComponentBOMLine) targets[0]).getItem().getType().equals(TypeConstant.S7_PREPRODUCTTYPE))
